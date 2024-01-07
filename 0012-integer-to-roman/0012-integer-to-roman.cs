b63aa8defb75f8e62d833befe47c09d2f2f9cb98
mohammedmudassir687687 <31137048+mohammedmudassir687687@users.noreply.github.com>
2024-01-07 13:18:43 -0600

    Time: 62 ms (84.92%), Space: 44.5 MB (41.60%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -0,0 +1,74 @@
+public class Solution {
+    public string IntToRoman(int num) {
+        
+        string res = "";
+        
+        while(num != 0) {
+       
+            while (num - 1000 >= 0) {
+                res += "M";
+                num -= 1000;
+            }
+            
+            while (num - 900 >= 0) {
+                res += "CM";
+                num -= 900;
+            }
+            
+            while (num - 500 >= 0) {
+                res += "D";
+                num -= 500;
+            }
+            
+            while (num - 400 >= 0) {
+                res += "CD";
+                num -= 400;
+            }
+            
+            while (num - 100 >= 0) {
+                res += "C";
+                num -= 100;
+            }
+            
+            while (num - 90 >= 0) {
+                res += "XC";
+                num -= 90;
+            }
+            
+            while (num - 50 >= 0) {
+                res += "L";
+                num -= 50;
+            }
+            
+            while (num - 40 >= 0) {
+                res += "XL";
+                num -= 40;
+            }
+            
+            while (num - 10 >= 0) {
+                res += "X";
+                num -= 10;
+            }
+            
+            while (num - 9 >= 0) {
+                res += "IX";
+                num -= 9;
+            }
+            while (num - 5 >= 0) {
+                res += "V";
+                num -= 5;
+            }
+            while (num - 4 >= 0) {
+                res += "IV";
+                num -= 4;
+            }
+            while (num > 0) {
+                res += 'I';
+                num -= 1;
+            }
+            
+        }
+        return res;
+        
+    }
+}
