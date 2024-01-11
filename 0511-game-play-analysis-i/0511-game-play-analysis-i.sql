e6769ef51a526996b4240c7632c534a0f9ba8732
mohammedmudassir687687 <31137048+mohammedmudassir687687@users.noreply.github.com>
2024-01-10 18:25:00 -0600

    Time: 893 ms (55.55%), Space: 0B (100.00%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+
+SELECT player_id, event_date AS first_login
+FROM (
+SELECT player_id, event_date,
+ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
+FROM Activity
+) AS sub
+WHERE rn = 1
