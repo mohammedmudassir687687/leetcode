2cc8757cc4d9221670d7f537a6fe6a327835ceb9
mohammedmudassir687687 <31137048+mohammedmudassir687687@users.noreply.github.com>
2024-01-15 14:47:23 -0600

    Time: 646 ms (59.06%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -14,14 +14,4 @@ ON t1.query_name = t2.query_name
 WHERE t1.query_name IS NOT NULL
 
 
-# SELECT query_name, quality, ROUND(100*lt3/total,2) AS poor_query_percentage
-# FROM
-# (SELECT query_name, ROUND(AVG(rating/position),2) AS quality, COUNT(*) AS total, 
-# CASE WHEN (SELECT COUNT(*) FROM Queries WHERE rating < 3 GROUP BY query_name) > 0
-# THEN COUNT(*)
-# ELSE 0
-# END AS lt3
-# FROM Queries
-# GROUP BY query_name) AS t1
-
 
